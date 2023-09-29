d009f581fe96c40897a3e1142a8416b7b99f9970
v.konovalov <v.konovalov@iskrauraltel.ru>
2023-09-29 17:25:54 +0500

    homework-zio_streams

--- classwork-akka_akka_streams/src/main/scala/akka_akka_streams/AkkaDataStreams/StartStop.scala
+++ classwork-akka_akka_streams/src/main/scala/akka_akka_streams/AkkaDataStreams/StartStop.scala
@@ -5,18 +5,22 @@ import akka.actor.{DeathPactException, Kill, SupervisorStrategy}
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 
-sealed  trait Command
+sealed trait Command
+
 case class StartChild(name: String) extends Command
+
 case class SendMessageToChild(name: String, msg: String, num: Int) extends Command
+
 case class StopChild(name: String) extends Command
+
 case object Stop extends Command
 
 object Parent {
   def apply(): Behavior[Command] = withChildren(Map())
 
   def withChildren(childs: Map[String, ActorRef[Command]]): Behavior[Command] =
-    Behaviors.setup( { ctx =>
-      Behaviors.receiveMessage{
+    Behaviors.setup({ ctx =>
+      Behaviors.receiveMessage {
         case StartChild(name) =>
           ctx.log.info(s"Start child $name")
           val newChild = ctx.spawn(Child(), name)
@@ -24,32 +28,32 @@ object Parent {
         case msg@SendMessageToChild(name, _, i) =>
           ctx.log.info(s"Send message to child $name num=$i")
           val childOption = childs.get(name)
-          childOption.foreach(childRef=> childRef ! msg)
+          childOption.foreach(childRef => childRef ! msg)
           Behaviors.same
         case StopChild(name) =>
           ctx.log.info(s"Stopping child with name $name")
           val childOption = childs.get(name)
           childOption match {
-            case Some(childRef)=>
+            case Some(childRef) =>
               ctx.stop(childRef)
               Behaviors.same
           }
       }
-    }
+    })
 }
 
 object Child {
   def apply(): Behavior[Command] = Behaviors.setup { ctx =>
-    Behaviors.receiveMessage{msg=>
+    Behaviors.receiveMessage { msg =>
       ctx.log.info(s"Child got message $msg")
       Behaviors.same
     }
   }
 }
 
-object StartStopSpec extends App{
+object StartStopSpec extends App {
   def apply(): Behavior[NotUsed] =
-    Behaviors.setup{ctx =>
+    Behaviors.setup { ctx =>
       val parent = ctx.spawn(Parent(), "parent")
       parent ! StartChild("child1")
       parent ! StartChild("child2")
@@ -61,6 +65,7 @@ object StartStopSpec extends App{
       //for (i <- 1 to 15) parent ! SendMessageToChild("child1", "massdnmbfsdmf", i)
       Behaviors.same
     }
+
   val value = StartStopSpec()
   implicit val system = ActorSystem(value, "akka_typed")
   Thread.sleep(5000)
--- classwork-akka_akka_streams/src/main/scala/zio/zioStreamsIntroduction.scala
+++ classwork-akka_akka_streams/src/main/scala/zio/zioStreamsIntroduction.scala
@@ -1,9 +1,65 @@
 package zio
 
-import ch.qos.logback.classic.{Level, Logger}
-import zio._
-import zio.kafka.consumer._
-import zio.kafka.producer.{Producer, ProducerSettings}
-import zio.kafka.serde._
-import zio.stream.ZStream
-import org.slf4j.LoggerFactory
+import zio.stream.{ZSink, ZStream}
+import zio.Console.printLine
+
+import scala.concurrent.TimeoutException
+
+object zioStreamsIntroduction extends App {
+  val emptyStream = ZStream.empty
+  val oneIntValue = ZStream.succeed(4)
+  val oneListValueStream = ZStream.succeed(List(1,2,3))
+  val infiniteIntStream = ZStream.iterate(1)(_+1)
+
+  val stream = ZStream(1,2,3)
+  val unit = ZStream.unit
+  val never = ZStream.never
+
+  val s1 = ZStream.fromChunk(Chunk(1,2,3))
+  val s2 = ZStream.fromChunks(Chunk(1,2,3), Chunk(4,5,6))
+  val s3 = ZStream(1,2,3) ++ ZStream.fail("error") ++ ZStream(4,5)
+  val s4 = ZStream(1,2,3)
+
+  val stream1 = s3.orElse(s4)
+
+  val s5 = ZStream(1,2,3) ++ ZStream.fail("error") ++ ZStream(4,5)
+  val s6 = ZStream(1,2,3)
+
+  val stream2 = s5.catchSome{
+    case "error" => s6
+    case "error1" => s4
+  }
+
+  val number = ZStream(1,2,3) ++
+    ZStream.fromZIO(
+      Console.print("Enter a number: ") *> Console.readLine
+        .flatMap(x=>
+          x.toIntOption match {
+            case Some(value) => ZIO.succeed(value)
+            case None => ZIO.fail("NaN")
+          }
+        )
+    ).retry(Schedule.exponential(1.seconds))
+
+  number.timeoutFail(new TimeoutException)(10.seconds)
+
+  val sum = ZStream(1,2,3,4).run(ZSink.sum)
+  val fold = ZStream(1,2,3,4).runFold(0)(_+_)
+  val forEach = ZStream(1,2,3,4).foreach(printLine(_))
+
+  val forever = ZStream.iterate(0)(_+1)
+  val take = forever.take(5)
+  val takeWhile = forever.takeWhile(_<5)
+  val takeUntil = forever.takeUntil(_<5)
+  val takeRight = forever.takeRight(3)
+
+  val intStream = ZStream.fromIterable(0 to 100)
+  val stringStream = intStream.map(_.toString)
+  val rangeFilter = ZStream.range(1, 11).filter(_%2==0)
+  val forCompr = for {
+    i <- ZStream.range(1, 11).take(10)
+    if i%2 ==0
+  } yield i
+  val filterNot = ZStream(1,2,3,4).filterNot(_%2==0)
+
+}
